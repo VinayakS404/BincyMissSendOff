d7327dd0c152654e038eb1ab350a8e346679e81e
Vinayak S <oreva.vinayak@gmail.com>
2026-01-19 04:06:19 +0530

    feat: complete the login page JS

--- src/js/loginPage.js
+++ src/js/loginPage.js
@@ -2,9 +2,9 @@ const eyeElement = document.querySelector(".eye-js");
 const siginElement = document.querySelector(".Sigin-1st-btn-js");
 const mainElement = document.querySelector(".main-js");
 const backElement = document.querySelector(".back-img-js");
-const back2ndElement = document.querySelector(".js-back-2nd-img");
+const back2ndElement = document.querySelector(".back-2nd-img-js");
 const forgotElement = document.querySelector(".forgot-p-js");
-const passwordElement = document.querySelector(".user-pass-inp-js");
+export const passwordElement = document.querySelector(".pass-inp-js");
 
 siginElement.addEventListener("click", () => {
   mainElement.classList.remove("main-2nd-onclick");
---
+++ src/js/loginpage2nd.js
@@ -0,0 +1,46 @@
+import { passwordElement } from "./loginPage.js";
+const usernameElement = document.querySelector(".user-inp-js");
+const btnElement = document.querySelector(".Sigin-2nd-btn-js");
+const userDivElement = document.querySelector(".user-div-js");
+const passDivElement = document.querySelector(".pass-div-js");
+
+const correctUsername = "a";
+const correctPassword = "s";
+
+btnElement.addEventListener("click", () => {
+  const enteredUsername = usernameElement.value;
+  const enteredPassword = passwordElement.value;
+  if (
+    enteredUsername === correctUsername &&
+    enteredPassword === correctPassword
+  ) {
+    // Both correct
+    window.location.href = "./home.html";
+  } else if (
+    enteredUsername !== correctUsername &&
+    enteredPassword !== correctPassword
+  ) {
+    // Both wrong
+    wronguser();
+    wrongpass();
+  } else if (enteredUsername !== correctUsername) {
+    // Only username wrong
+    wronguser();
+  } else if (enteredPassword !== correctPassword) {
+    // Only password wrong
+    wrongpass();
+  }
+});
+
+function wrongpass() {
+  passDivElement.classList.add("user-pass-div-wrong-user");
+  setTimeout(() => {
+    passDivElement.classList.remove("user-pass-div-wrong-user");
+  }, 500);
+}
+function wronguser() {
+  userDivElement.classList.add("user-pass-div-wrong-user");
+  setTimeout(() => {
+    userDivElement.classList.remove("user-pass-div-wrong-user");
+  }, 500);
+}
